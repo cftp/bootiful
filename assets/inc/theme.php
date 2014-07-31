f36bc307063cd02f52a1a9f324e6910d914b2ba0
Scott Evans <git@scott.ee>
2014-07-31 10:29:10 +0100

    Bootstrap comment styles

--- assets/inc/theme.php
+++ assets/inc/theme.php
@@ -284,3 +284,48 @@ function cftp_embed_tweaks($provider, $args, $url) {
 	return $provider;
 }
 add_filter('oembed_fetch_url', 'cftp_embed_tweaks', 30, 3);
+
+/**
+ * cftp_bootstrap_comment_fields
+ *
+ * Use bootstrap compatible HTML for comments
+ *
+ * @param  array $fields
+ * @return array
+ */
+function cftp_bootstrap_comment_fields( $fields ) {
+	$commenter = wp_get_current_commenter();
+
+	$req      = get_option( 'require_name_email' );
+	$aria_req = ( $req ? " aria-required='true'" : '' );
+	$html5    = current_theme_supports( 'html5', 'comment-form' ) ? 1 : 0;
+
+	$fields   =  array(
+		'author' => '<div class="form-group comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+		'<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></div>',
+		'email'  => '<div class="form-group comment-form-email"><label for="email">' . __( 'Email' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label> ' .
+		'<input class="form-control" id="email" name="email" ' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></div>',
+		'url'    => '<div class="form-group comment-form-url"><label for="url">' . __( 'Website' ) . '</label> ' .
+		'<input class="form-control" id="url" name="url" ' . ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></div>',
+	);
+
+	return $fields;
+}
+add_filter( 'comment_form_default_fields', 'cftp_bootstrap_comment_fields' );
+
+/**
+ * cftp_bootstrap_comment_textarea
+ *
+ * Use bootstrap compatible HTML for comments
+ *
+ * @param  array $args
+ * @return array
+ */
+function cftp_bootstrap_comment_textarea( $args ) {
+	$args['comment_field'] = '<div class="form-group comment-form-comment">
+			<label for="comment">' . _x( 'Comment', 'noun' ) . '</label>
+			<textarea class="form-control" id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
+		</div>';
+	return $args;
+}
+add_filter( 'comment_form_defaults', 'cftp_bootstrap_comment_textarea' );
